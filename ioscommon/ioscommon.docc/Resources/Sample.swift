25361fae20cd4a6086c0c84fced8722995ba403f
Cuongnv <Cuongnv219@gmail.com>
2021-11-12 18:16:07 +0700

    Update

---
+++ ioscommon/ioscommon.docc/Resources/Sample.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Katana on 12/11/2021.
+//
+
+import Foundation
+
+
---
+++ ioscommon/ioscommon.h
@@ -0,0 +1,18 @@
+//
+//  ioscommon.h
+//  ioscommon
+//
+//  Created by Katana on 12/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ioscommon.
+FOUNDATION_EXPORT double ioscommonVersionNumber;
+
+//! Project version string for ioscommon.
+FOUNDATION_EXPORT const unsigned char ioscommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ioscommon/PublicHeader.h>
+
+
